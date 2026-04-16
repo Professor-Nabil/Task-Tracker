b55ec1d991cd27f1244658c277cf40a7b0460cdf
Nabil <Nabil@gmail.com>
2026-04-16 17:20:07 +0100

    tree

--- wiki/Tree.sh
+++ wiki/Tree.sh
@@ -1,11 +1,56 @@
-./src/app.js
+.
+├── bin
+│   └── task-cli.js
+├── data
+│   ├── tasks.json
+│   └── tasks.json.example
+├── package.json
+├── README.md
+└── src
+    ├── app.js
+    ├── assets
+    │   └── help.json
+    ├── controllers
+    │   ├── controller-add.js
+    │   ├── controller-delete.js
+    │   ├── controller-help.js
+    │   ├── controller-list.js
+    │   ├── controller-mark.js
+    │   └── controller-update.js
+    ├── middlewares
+    │   ├── validate-argv-length.js
+    │   ├── validate-description.js
+    │   ├── validate-id.js
+    │   └── validate-status.js
+    ├── models
+    │   └── Task-Model.js
+    ├── repository
+    │   ├── read.js
+    │   └── write.js
+    ├── routes
+    │   └── router-commands-inputs.js
+    ├── services
+    │   ├── service-add-task.js
+    │   ├── service-delete-task.js
+    │   ├── service-help-page.js
+    │   ├── service-list-tasks.js
+    │   ├── service-mark-task.js
+    │   └── service-update-task.js
+    ├── utils
+    │   ├── cli
+    │   │   └── colors.js
+    │   ├── date
+    │   │   └── get-local-time.js
+    │   ├── errors
+    │   │   └── AppError.js
+    │   └── services
+    │       ├── auto-increment-id.js
+    │       ├── delete-task-from-array.js
+    │       └── update-task-in-array.js
+    └── views
+        ├── print-fail-operation.js
+        ├── print-help-page.js
+        ├── print-success-operation.js
+        └── view-list-tasks.js
 
-./src/routes/router-commands-inputs.js
-
-./src/middlewares/validate-argv-length.js
-
-./src/controllers/controller-help.js
-
-./src/services/service-help-page.js
-
-./src/views/print-fail-operation.js
+17 directories, 37 files
