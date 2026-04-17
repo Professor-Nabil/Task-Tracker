967a3944169ae2b1ebd02d202c0d4925a9fce7e6
Nabil <Nabil@gmail.com>
2026-04-17 18:48:18 +0100

    feat(repo): integrate help assets into central config
    - Moved help.json paths to db-config.js
    - Refactored service-help-page to use dynamic constants
    - Cleaned up hardcoded path strings in the service layer

--- src/repository/db-config.js
+++ src/repository/db-config.js
@@ -7,3 +7,7 @@ export const DATA_FOLDER = "data";
 export const DB_FILE = isTest ? "tasks.test.json" : "tasks.json";
 
 export const FULL_PATH = path.resolve(DATA_FOLDER, DB_FILE);
+
+// This is for  Help Asset path (Help Page)
+export const ASSETS_FOLDER = path.resolve("src", "assets");
+export const HELP_FILE = "help.json";
--- src/services/service-help-page.js
+++ src/services/service-help-page.js
@@ -1,16 +1,15 @@
 import { AppError } from "../utils/errors/AppError.js";
 import { read } from "../repository/read.js";
 import { join } from "node:path";
-// 1. Import the dynamic names
-// import { DATA_FOLDER, DB_FILE } from "../repository/db-config.js";
+import { ASSETS_FOLDER, HELP_FILE } from "../repository/db-config.js";
 
-const path = "./src/assets/";
-const file = "help.json";
-const DATA_FILE = join(path, file);
+// const path = "./src/assets/";
+// const file = "help.json";
+const DATA_FILE = join(ASSETS_FOLDER, HELP_FILE);
 
 export const serviceHelpPage = async (cliInputs) => {
   try {
-    let result = await read(path, file);
+    let result = await read(ASSETS_FOLDER, HELP_FILE);
     if (result == null) {
       throw new AppError(`ERROR: Help page not found ${DATA_FILE}`, "ENOENT");
     }
